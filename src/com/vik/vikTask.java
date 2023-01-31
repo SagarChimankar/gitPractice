fc3f0ee80c3485560d6e423e5476f69ea61713fc
Vikrant <vikrantgopewar@gmail.com>
2023-01-31 17:58:09 +0530

    vik commit

---
+++ src/com/vik/vikTask.java
@@ -0,0 +1,11 @@
+package com.vik;
+
+public class vikTask {
+	// create program to print 1 to 10
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
